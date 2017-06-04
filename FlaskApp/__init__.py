22ecbd097750365e24beaf60f1b84d428100778a
ksizzle88 <kmschepis@gmail.com>
2017-06-04 02:13:40 +0000

    init

---
+++ FlaskApp/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("main.html")
+
+@app.route('/ocr/')
+def ocrpage():
+    return render_template("ocr.html")
+
+
+if __name__ == "__main__":
+    app.run()
