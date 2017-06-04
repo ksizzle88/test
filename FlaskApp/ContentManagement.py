c75f8396979423de9cf28d0e310076cb9fee19e7
ksizzle88 <kmschepis@gmail.com>
2017-06-04 23:43:39 +0000

    completed tutorial 9

--- FlaskApp/ContentManagement.py
+++ FlaskApp/ContentManagement.py
@@ -1,12 +1,12 @@
 def Content(): 
     TOPIC_DICT = {
     "Basics": [
-    ["Introduction", "/introduction-"     "to-python-programming/"],
+    ["Introduction", "/introduction-to-python-programming/"],
     ["Print Functions and Strings", "/Print-Functions-and-Strings]"], 
     ["Math Basics with Python 3", "Math-Basics-with-Python-3"],
     ],
-    "Webdev": [
-    ["intr to web dev", "/intro-to-web-dev/"]
+    "Web dev": [
+    ["Intro to Web Dev", "/intro-to-web-dev/"]
     ]
     }
     return TOPIC_DICT
--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask,render_template
+from ContentManagement import Content
+
+TOPIC_DICT = Content()
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def homepage():
-    return render_template("main.html")
+	try:
+		return render_template("main.html")
+	except Exception as e:
+		return str(e)
+    
 
 @app.route('/ocr/')
 def ocrpage():
@@ -12,7 +20,18 @@ def ocrpage():
 
 @app.route('/dashboard/')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", TOPIC_DICT = TOPIC_DICT)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html")
+
+@app.route('/slashboard/')		
+def slashboard():
+    try:
+        return render_template("dashboard.html", TOPIC_DICT = shamwow)
+    except Exception as e:
+	    return render_template("error.html", error = str(e))
 
 if __name__ == "__main__":
     app.run()
